3d3ed043d5b8deea2ef4a5c51a390c3942401ea6
Mehdy Faik <faik.mehdy@gmail.com>
2022-04-08 12:53:29 -0600

    nodebench and library both build with c++20. here goes nodebench testing of the new semaphore ringbuffer.

--- src/Application/NodeBench/main.cpp
+++ src/Application/NodeBench/main.cpp
@@ -417,7 +417,7 @@ int main(int argc, char** argv)
 
   std::cout << "nodebench: waiting for nodes to run..." << std::endl;
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+  std::this_thread::sleep_for(std::chrono::milliseconds(10000));
 
   std::cout << "nodebench: nodes done running" << std::endl;
 
